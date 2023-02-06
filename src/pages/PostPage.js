b4810281fe0bd2aae0ae50bb6c25232dc4efcf54
Danny <44902431+DatoKhojava@users.noreply.github.com>
2023-02-07 00:37:50 +0400

    deleted unnecessary imports

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout } from "../components/Layout";
-import { Routes, Route, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function PostPage() {
   let { id } = useParams();
